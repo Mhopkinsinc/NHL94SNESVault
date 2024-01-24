24de893ce1f6fd575262acb43c8fc3a0e979a47b
MHopkins <mhopkinsinc@gmail.com>
2024-01-23 22:05:07 -0500

    Code Refactor - Make modifications for this project to build on Mac OS and Linux Added asar bindings, updated extensions.json to include some additional extension that may be need, modified tasks.json to run different commands per OS (Still needs to be tested)

--- Src/config.asm
+++ Src/config.asm
@@ -1,19 +1,19 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;This Sets Defense Control On or Off by Default
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-!Defense_Control = 0        ; Off = 0 On = 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+!Defense_Control = 0        ; Enable = 1,  Disable = 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Enable Hidden "30 Seconds" Periods Selection in Menu
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-!Thirty_Second_Period = 0     ; Enable = 1,  Disable = 0
+!Thirty_Second_Period = 0   ; Enable = 1,  Disable = 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Enable Pull Goalie With L+R Trigger
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-!Manual_Pull_Goalie = 1       ; Enable = 1,  Disable = 0
+!Manual_Pull_Goalie = 1     ; Enable = 1,  Disable = 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Enable Real Time Clock 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-!Real_Time_Clock = 0       ; Enable = 01,  Disable = 0
+!Real_Time_Clock = 0        ; Enable = 1,  Disable = 0
