e2f2a4f21a0ada56b3e8f11b62bcd002297093f8
MHopkins <mhopkinsinc@gmail.com>
2024-02-25 19:50:06 -0500

    Hack Request - Remove/Limit Goalie Interference Fixes #31

--- Src/patches.asm
+++ Src/patches.asm
@@ -7,5 +7,7 @@
                        incsrc "patches/ManualPullGoalie.asm"
                        incsrc "patches/30SecondPeriods.asm"
                        incsrc "patches/RealTimeClock.asm"
+                       incsrc "patches/Interference.asm"
                        incsrc "patches/OneMinutePenalties.asm"
                        incsrc "patches/DefControl.asm"
+                       
