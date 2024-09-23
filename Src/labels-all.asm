958a0786bce40aa7e52a473617ccb0ee976d5e14
Mark <mhopkinsinc@gmail.com>
2024-09-22 22:47:23 -0400

    Mhopkinsinc/issue10 (#59)
    * Bug - Extra Attacker missing in certain situations after pulling goalie
    Fixes #10
    
    * fixed missing code from check in
    
    * added missing code
    
    * Merge remote-tracking branch 'origin/main' into Mhopkinsinc/issue10
    
    * Added missing code
    
    * Added missing code
    
    * Added Missing code
    
    * Merge branch 'main' into Mhopkinsinc/issue10

--- Src/labels-all.asm
+++ Src/labels-all.asm
@@ -55,4 +55,5 @@
                        CODE_9EC06D = $9EC06D
                        jmpToGoalLogic = $9E920F
                        PStructRNG = $9E1A12; !^ Random Number Generator for Attributes ^!
-                       CODE_9B8D72 = $9B8D72                       
+                       CODE_9B8D72 = $9B8D72
+                       CODE_9B8D7C = $9B8D7C
